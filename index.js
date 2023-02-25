5e37cdcebbf8d1079c2ce81898ca42e15fa2f306
SaraKaterji <sarah1_k1@hotmail.com>
2023-02-25 19:53:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ function excelFileToJSON(file, searchstring) {
 }
 
 function lookupJson(lookuptable, messages, searchstring) {
+    searchstring = searchstring.trim().toUpperCase();
     var energielabel = document.getElementById("energielabel");
     var message = document.getElementById("message");
     var found = false;
